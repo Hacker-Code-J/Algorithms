049903b60f0d510418eead1d88cf67930fd3af78
Hacker-Code-J <hacker3740@gmail.com>
2023-10-30 16:21:52 +0900

    Laptop231030v1.3

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include "algorithms.h"
 
 int main() {
-    int A[] = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+    // int A[] = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+    // int A[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};   // Best case
+    int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};      // Worst case
     int n = sizeof(A) / sizeof(A[0]);
 
     printf("Original array: ");
